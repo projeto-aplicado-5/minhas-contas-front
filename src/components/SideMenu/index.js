1cbdf9564eae1e1254ff8c71e53b3d07bd392a05
gustavodaltoe <gustavodaltoe@hotmail.com>
2020-05-15 02:52:02 -0300

    Layout do header

--- src/components/SideMenu/index.js
+++ src/components/SideMenu/index.js
@@ -1,18 +1,61 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Link, useRouteMatch } from 'react-router-dom';
+import { FaEye, FaEyeSlash } from 'react-icons/fa'
 
-import './side-menu.css'
+import './side-menu.css';
 
 export default function Accounts() {
+    
+    const [ showSummary, setShowSummary ] = useState(true);
+
+    useEffect(() => {
+        const storageShowSummary = window.localStorage.getItem('showSummary');  
+        const boolValue = storageShowSummary === 'true' || storageShowSummary === null;
+        setShowSummary(boolValue);
+    }, [])
+    
+    const { path } = useRouteMatch();
+
+    const handleEyeClick = event => {
+        window.localStorage.setItem('showSummary', !showSummary);
+        setShowSummary(!showSummary);
+    };
+
     return (
         <aside id="side-menu">
             <section className="menu-header">
-                Oi, eu sou o menu.
+                <h1>Minhas Contas</h1>
+                <p>Bem vindo, Kakaroto</p>
             </section>
             <nav>
-                Itens do menu.
+                <ul>
+                    <li className={path === '/lançamentos' ? 'active' : ''}>
+                        <Link to="/lançamentos">Histórico de Lançamentos</Link>
+                    </li>
+                    <li className={path === '/adicionar-lançamento' ? 'active' : ''}>
+                        <Link to="/adicionar-lançamento">Adicionar Lançamento</Link>
+                    </li>
+                    <li className={path === '/contas' ? 'active' : ''}>
+                        <Link to="/contas">Contas</Link>
+                    </li>
+                    <li className={path === '/categorias' ? 'active' : ''}>
+                        <Link to="/categorias">Categorias</Link>
+                    </li>
+                </ul>
             </nav>
             <section className="menu-footer">
-                Rodapé do menu.
+                <div className="account-summary">
+                    <small>R$</small>
+                    <h3>
+                        { showSummary ? 
+                        '2.225,00' : 
+                        <span>( ͡~ ͜ʖ ͡°)</span> }
+                    </h3>
+                    <button onClick={ handleEyeClick }>
+                        {showSummary ? <FaEye /> : <FaEyeSlash />}
+                    </button>
+                </div>
+                <p>{ showSummary ? 'Seu saldo geral' : 'shhhh'}</p>
             </section>
         </aside>
     );
