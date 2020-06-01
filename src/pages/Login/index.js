56932241408db7ddd95390cca01dbc354112c973
Paulorhg <30507591+Paulorhg@users.noreply.github.com>
2020-06-01 19:27:09 -0300

    ajustes login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom';
 
 import api from '../../services/api'
+import {AiOutlineRollback} from 'react-icons/ai'
 
 import './login.css'
 
@@ -13,11 +14,15 @@ export default function Accounts() {
         <div id="login">
             <form id="logar" action="">
                 <legend>Fazer Login</legend>
-                <label htmlFor="nome">Nome Usuario</label>
-                <input id="nome" type="text"/>
+                <label htmlFor="email">Email do Usuario</label>
+                <input id="email" type="email" required/>
                 <label htmlFor="senha">Senha</label>
-                <input id="senha" type="text"/>
+                <input id="senha" type="text" required/>
                 <button type="submit">Entrar</button>
+                <Link className="voltar" to="/">
+                    <AiOutlineRollback size={16}/>
+                    Voltar
+                </Link>
             </form>
         </div>
     );
