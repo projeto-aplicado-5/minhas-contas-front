cbd72a8aa74ff598f50586f7fdbb262df4de36dc
Paulorhg <30507591+Paulorhg@users.noreply.github.com>
2020-06-01 18:22:44 -0300

    feito Categorias estatico

--- src/pages/Categories/index.js
+++ src/pages/Categories/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 
 import Template from '../../components/Template' 
-
+import {AiFillPlusSquare} from 'react-icons/ai'
+import {FiTrash2} from 'react-icons/fi'
 import api from '../../services/api'
 
 import './categories.css'
@@ -11,7 +12,32 @@ export default function Categories() {
 
     return (
         <Template>
-            <h1>Oi, eu sou o conteudo de categorias.</h1>
+            <div id="categorias">
+                <h1>CATEGORIAS</h1>
+
+                <form id="addCat" action="" method="post">
+                    <label htmlFor="novaCat">Descrição</label><br/>
+                    <input id="novaCat" type="text"/>
+                    <button type="submit">
+                        <AiFillPlusSquare size={32} color="#1AD274" />
+                    </button>
+                </form>
+
+                <ul id="catCadastradas">
+                    <li>
+                        <p>Renda</p>
+                        <button>
+                            <FiTrash2 size={28} color="#FFFFFF"/>
+                        </button>
+                    </li>
+                    <li>
+                        <p>Comida</p>
+                        <button>
+                            <FiTrash2 size={28} color="#FFFFFF"/>
+                        </button>
+                    </li>
+                </ul>
+            </div>
         </Template>
     );
 }
